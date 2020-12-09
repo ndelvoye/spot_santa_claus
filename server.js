b2a98ea8f562e85a0339034392c9193b2fb468e1
ndelvoye <nicolhas.delvoye@gmail.com>
2020-12-09 12:08:59 +0100

    server.js modfied to give static files

--- server.js
+++ server.js
@@ -9,25 +9,23 @@ let sweden = {
 
 let mySanta = new Santa(sweden);
 
-
 const express = require('express');
-const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const app = express();
+app.disable("x-powered-by")
+app.use(express.static('public'))
+app.use('/images', express.static(__dirname + '/images'))
+app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
-
+const io = require('socket.io')(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 setInterval( () => io.emit('time', new Date().toTimeString()), 1000);
-
 setInterval( () => io.emit('santa', JSON.stringify(mySanta.getPosition())), 1000);
-
